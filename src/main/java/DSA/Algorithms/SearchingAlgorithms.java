50e95bf84c0fad911c90fc0ebddff1f0c60dca98
sashank17 <sashank17102002@yahoo.in>
2023-05-16 21:19:35 +0530

    Implemented Sentinel Linear Search

--- src/main/java/DSA/Algorithms/SearchingAlgorithms.java
+++ src/main/java/DSA/Algorithms/SearchingAlgorithms.java
@@ -2,13 +2,70 @@ package DSA.Algorithms;
 
 public class SearchingAlgorithms {
 
-    /**
-     * Function to determine if an element exists in a sorted array or not using Iterative Binary Search Algorithm
-     * @param nums
-     * @param target
-     * @return
-     */
-    public static int binarySearchItr(int[] nums, int target)
+    // Function to determine if a `target` exists in the unsorted array `nums` or not using Linear Search Algorithm
+    public static int linearSearch(int[] nums, int target) {
+
+        // loop over the entire search space
+        for (int i = 0; i < nums.length; i++) {
+
+            // `target` is found
+            if (target == nums[i]) {
+                return i;
+            }
+        }
+
+        // `target` doesn't exist in the array
+        return -1;
+    }
+
+
+    // Function to determine if an element exists in an array or not using the Sentinel Linear Search Algorithm
+    public static int sentinelLinearSearch(int nums[], int target)
+    {
+        int n = nums.length;
+
+        // Last element of the array
+        int last = nums[n - 1];
+
+        // Element to be searched is placed at the last index
+        nums[n - 1] = target;
+        int i = 0;
+
+        while (nums[i] != target) {
+            i++;
+        }
+
+        // Put the last element back
+        nums[n - 1] = last;
+
+        // `target` is found
+        if ((i < n - 1) || (nums[n - 1] == target))
+            return i;
+
+        // `target` doesn't exist in the array
+        return -1;
+    }
+
+
+    // Function to determine if a `target` exists in the sorted array `nums` or not using Linear Search Algorithm
+    public static int sortedLinearSearch(int[] nums, int target) {
+
+        // loop over the entire search space
+        for (int i = 0; nums[i] <= target; i++) {
+
+            // `target` is found
+            if (target == nums[i]) {
+                return i;
+            }
+        }
+
+        // `target` doesn't exist in the array
+        return -1;
+    }
+
+
+    // Function to determine if an element exists in a sorted array or not using Iterative Binary Search Algorithm
+    public static int iterativeBinarySearch(int[] nums, int target)
     {
         int left = 0, right = nums.length - 1;
 
@@ -16,7 +73,7 @@ public class SearchingAlgorithms {
         while (left <= right)
         {
             // find the mid-value in the search space and compare it with the target
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
 
             // `target` is found
             if (target == nums[mid]) {
@@ -39,15 +96,8 @@ public class SearchingAlgorithms {
     }
 
 
-    /**
-     * Function to determine if an element exists in a sorted array or not using a recursive implementation of the Binary Search Algorithm
-     * @param nums
-     * @param left
-     * @param right
-     * @param target
-     * @return
-     */
-    public static int binarySearchRec(int[] nums, int left, int right, int target)
+    // Function to determine if an element exists in a sorted array or not using a recursive implementation of the Binary Search Algorithm
+    public static int recursiveBinarySearch(int[] nums, int left, int right, int target)
     {
         // Base condition (search space is exhausted)
         if (left > right) {
@@ -55,7 +105,7 @@ public class SearchingAlgorithms {
         }
 
         // find the mid-value in the search space and compare it with the target
-        int mid = (left + right) / 2;
+        int mid = left + (right - left) / 2;
 
         // Base condition (a target is found)
         if (target == nums[mid]) {
@@ -64,46 +114,16 @@ public class SearchingAlgorithms {
 
         // discard all elements in the right search space, including the middle element
         else if (target < nums[mid]) {
-            return binarySearchRec(nums, left, mid - 1, target);
+            return recursiveBinarySearch(nums, left, mid - 1, target);
         }
 
         // discard all elements in the left search space, including the middle element
         else {
-            return binarySearchRec(nums, mid + 1, right, target);
+            return recursiveBinarySearch(nums, mid + 1, right, target);
         }
     }
 
 
-    // Function to determine if a `target` exists in the unsorted array `nums` or not using Linear Search Algorithm
-    public static int linearSearch(int[] nums, int target) {
-
-        // loop over the entire search space
-        for (int i = 0; i < nums.length; i++) {
-
-            // `target` is found
-            if (target == nums[i]) {
-                return i;
-            }
-        }
-
-        // `target` doesn't exist in the array
-        return -1;
-    }
 
 
-    // Function to determine if a `target` exists in the sorted array `nums` or not using Linear Search Algorithm
-    public static int linearSearchSorted(int[] nums, int target) {
-
-        // loop over the entire search space
-        for (int i = 0; nums[i] <= target; i++) {
-
-            // `target` is found
-            if (target == nums[i]) {
-                return i;
-            }
-        }
-
-        // `target` doesn't exist in the array
-        return -1;
-    }
 }
