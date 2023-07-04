0d6c700e40042ac634a958d3bcf11cc47b66e434
sashank17 <sashank17102002@gmail.com>
2023-07-04 13:54:20 +0530

    Fixed comments

--- src/main/java/DSA/Algorithms/SearchingAlgorithms.java
+++ src/main/java/DSA/Algorithms/SearchingAlgorithms.java
@@ -2,32 +2,32 @@ package DSA.Algorithms;
 
 public class SearchingAlgorithms {
 
-    // Function to determine if a `target` exists in the unsorted array `nums` or not using Linear Search Algorithm
+    // Function to determine if an element exists in an unsorted array or not using Linear Search Algorithm
     public static int linearSearch(int[] nums, int target) {
 
         // loop over the entire search space
         for (int i = 0; i < nums.length; i++) {
 
-            // `target` is found
+            // target is found
             if (target == nums[i]) {
                 return i;
             }
         }
 
-        // `target` doesn't exist in the array
+        // target doesn't exist in the array
         return -1;
     }
 
 
     // Function to determine if an element exists in an array or not using the Sentinel Linear Search Algorithm
-    public static int sentinelLinearSearch(int nums[], int target)
+    public static int sentinelLinearSearch(int[] nums, int target)
     {
         int n = nums.length;
 
-        // Last element of the array
+        // last element of the array
         int last = nums[n - 1];
 
-        // Element to be searched is placed at the last index
+        // element to be searched is placed at the last index
         nums[n - 1] = target;
         int i = 0;
 
@@ -35,31 +35,31 @@ public class SearchingAlgorithms {
             i++;
         }
 
-        // Put the last element back
+        // put the last element back
         nums[n - 1] = last;
 
-        // `target` is found
+        // target is found
         if ((i < n - 1) || (nums[n - 1] == target))
             return i;
 
-        // `target` doesn't exist in the array
+        // target doesn't exist in the array
         return -1;
     }
 
 
-    // Function to determine if a `target` exists in the sorted array `nums` or not using Linear Search Algorithm
+    // Function to determine if an element exists in a sorted array or not using Linear Search Algorithm
     public static int sortedLinearSearch(int[] nums, int target) {
 
         // loop over the entire search space
         for (int i = 0; nums[i] <= target; i++) {
 
-            // `target` is found
+            // target is found
             if (target == nums[i]) {
                 return i;
             }
         }
 
-        // `target` doesn't exist in the array
+        // target doesn't exist in the array
         return -1;
     }
 
@@ -75,7 +75,7 @@ public class SearchingAlgorithms {
             // find the mid-value in the search space and compare it with the target
             int mid = left + (right - left) / 2;
 
-            // `target` is found
+            // target is found
             if (target == nums[mid]) {
                 return mid;
             }
@@ -91,7 +91,7 @@ public class SearchingAlgorithms {
             }
         }
 
-        // `target` doesn't exist in the array
+        // target doesn't exist in the array
         return -1;
     }
 
@@ -99,7 +99,7 @@ public class SearchingAlgorithms {
     // Function to determine if an element exists in a sorted array or not using a recursive implementation of the Binary Search Algorithm
     public static int recursiveBinarySearch(int[] nums, int left, int right, int target)
     {
-        // Base condition (search space is exhausted)
+        // base condition (search space is exhausted)
         if (left > right) {
             return -1;
         }
@@ -107,7 +107,7 @@ public class SearchingAlgorithms {
         // find the mid-value in the search space and compare it with the target
         int mid = left + (right - left) / 2;
 
-        // Base condition (a target is found)
+        // base condition (a target is found)
         if (target == nums[mid]) {
             return mid;
         }
@@ -124,15 +124,16 @@ public class SearchingAlgorithms {
     }
 
 
+    // Function to determine if an element exists in a sorted array or not using Ternary Search Algorithm
     public static int ternarySearch(int[] nums, int left, int right, int target)
     {
         if (right >= left) {
 
-            // Find the mid1 and mid2
+            // find the mid1 and mid2
             int mid1 = left + (right - left) / 3;
             int mid2 = right - (right - left) / 3;
 
-            // Check if target is present at any mid
+            // check if target is present at any mid
             if (nums[mid1] == target) {
                 return mid1;
             }
@@ -140,15 +141,17 @@ public class SearchingAlgorithms {
                 return mid2;
             }
 
-            // The target lies in between left and mid1
+            // the target lies in between left and mid1
             if (target < nums[mid1]) {
                 return ternarySearch(nums, left, mid1 - 1, target);
             }
-            // The target lies in between mid2 and right
+
+            // the target lies in between mid2 and right
             else if (target > nums[mid2]) {
                 return ternarySearch(nums, mid2 + 1, right, target);
             }
-            // The target lies in between mid1 and mid2
+
+            // the target lies in between mid1 and mid2
             else {
                 return ternarySearch(nums, mid1 + 1, mid2 - 1, target);
             }
